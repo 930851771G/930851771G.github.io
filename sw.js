6618943aa761c86e9755c70ca876334b65366487
gxy <930851771@qq.com>
2025-10-22 20:03:35 +0800

    Site updated: 2025-10-22 20:03:33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761133884101";const preCache = ["/images/taichi.png","/images/jf1021.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761134612392";const preCache = ["/images/taichi.png","/images/jf1021.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
